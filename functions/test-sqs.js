b0c3c6d3bccc94954646fe3702a26dd90db2682a
Duwab <contact@superpaas.pro>
2019-11-20 19:54:35 +0100

    Testing SQS 2/2

--- functions/test-sqs.js
+++ functions/test-sqs.js
@@ -1,8 +1,39 @@
+const AWS = require("aws-sdk");
+
 exports.handler = function(event, context, callback) {
   const { user } = context.clientContext;
-  const { AWS_IAM_ID, AWS_IAM_SECRET } = process.env;
+  const { AWS_IAM_ID, AWS_IAM_SECRET, AWS_SQS_QUEUE_URL } = process.env;
 
   console.log("testing log here");
+  if (AWS) {
+    console.log("aws sdk ok");
+    const sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
+    const params = {
+      DelaySeconds: 10,
+      MessageAttributes: {
+        // "Author": {
+        //     DataType: "String",
+        //     StringValue: "John Grisham"
+        // },
+        // "WeeksOn": {
+        //     DataType: "Number",
+        //     StringValue: "6"
+        // }
+      },
+      MessageBody: "Some body from Netlify",
+      // MessageDeduplicationId: "TheWhistler",  // Required for FIFO queues
+      // MessageId: "Group1",  // Required for FIFO queues
+      QueueUrl: AWS_SQS_QUEUE_URL || ""
+    };
+
+    sqs.sendMessage(params, (err, data) => {
+      if (err) {
+        console.log("error sending message", err);
+      } else {
+        console.log("Success", data.MessageId);
+      }
+    });
+  }
 
   if (user) {
     if (AWS_IAM_ID && AWS_IAM_SECRET) {
