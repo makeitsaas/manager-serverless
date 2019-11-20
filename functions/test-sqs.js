d805161457ed15fe67fd0ad26509493715e69351
Duwab <contact@superpaas.pro>
2019-11-20 20:03:22 +0100

    Fix SQS

--- functions/test-sqs.js
+++ functions/test-sqs.js
@@ -1,45 +1,40 @@
 const AWS = require("aws-sdk");
+const { AWS_IAM_ID, AWS_IAM_SECRET, AWS_SQS_QUEUE_URL } = process.env;
+
+AWS.config.update({
+  region: "eu-central-1",
+  accessKeyId: AWS_IAM_ID,
+  secretAccessKey: AWS_IAM_SECRET
+});
 
 exports.handler = function(event, context, callback) {
   const { user } = context.clientContext;
-  const { AWS_IAM_ID, AWS_IAM_SECRET, AWS_SQS_QUEUE_URL } = process.env;
-
-  console.log("testing log here");
-  if (AWS) {
-    console.log("aws sdk ok");
-    const sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
-    const params = {
-      DelaySeconds: 10,
-      MessageAttributes: {
-        // "Author": {
-        //     DataType: "String",
-        //     StringValue: "John Grisham"
-        // },
-        // "WeeksOn": {
-        //     DataType: "Number",
-        //     StringValue: "6"
-        // }
-      },
-      MessageBody: "Some body from Netlify",
-      // MessageDeduplicationId: "TheWhistler",  // Required for FIFO queues
-      // MessageId: "Group1",  // Required for FIFO queues
-      QueueUrl: AWS_SQS_QUEUE_URL || ""
-    };
-
-    sqs.sendMessage(params, (err, data) => {
-      if (err) {
-        console.log("error sending message", err);
-      } else {
-        console.log("Success", data.MessageId);
-      }
-    });
-  }
 
   if (user) {
-    if (AWS_IAM_ID && AWS_IAM_SECRET) {
-      callback(null, {
-        statusCode: 200,
-        body: AWS_IAM_ID.slice(0, 4) + "*********"
+    if (AWS_IAM_ID && AWS_IAM_SECRET && AWS_SQS_QUEUE_URL) {
+      const sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
+      const params = {
+        DelaySeconds: 10,
+        MessageAttributes: {},
+        MessageBody: "Some body from Netlify",
+        QueueUrl: AWS_SQS_QUEUE_URL
+      };
+
+      sqs.sendMessage(params, (err, data) => {
+        if (err) {
+          console.log("********** error sending message");
+          console.log(err);
+          console.log("********** end error");
+          callback(null, {
+            statusCode: 500,
+            body: JSON.stringify(err)
+          });
+        } else {
+          callback(null, {
+            statusCode: 200,
+            body: "Message: " + data.MessageId
+          });
+        }
       });
     } else {
       callback(null, {
