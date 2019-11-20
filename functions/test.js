73b82f1757b843bf6f5d3bb83efe7ca03f6fe5e9
Duwab <contact@superpaas.pro>
2019-11-20 19:19:06 +0100

    Test authentication within function

--- functions/test.js
+++ functions/test.js
@@ -1,7 +1,11 @@
 exports.handler = function(event, context, callback) {
+  const { user } = context.clientContext;
   // your server-side functionality
   callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body: {
+      message: "This is an authentication test",
+      user
+    }
   });
 };
