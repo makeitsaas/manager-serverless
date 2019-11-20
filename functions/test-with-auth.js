c9566bbe0b67fcde495dc5daf37579bf62804976
Duwab <contact@superpaas.pro>
2019-11-20 19:25:08 +0100

    Send full context

--- functions/test-with-auth.js
+++ functions/test-with-auth.js
@@ -1,11 +1,11 @@
 exports.handler = function(event, context, callback) {
-  const { user } = context.clientContext;
+  // const { user } = context.clientContext;
   // your server-side functionality
   callback(null, {
     statusCode: 200,
     body: {
       message: "This is an authentication test",
-      user
+      context
     }
   });
 };
